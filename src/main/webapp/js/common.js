89f35a9933dc9f1dd87f9839ad90bdf206006ef8
seungriyu <siso1210@naver.com>
2018-08-21 16:36:42 +0900

    ss

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -1,10 +1,9 @@
 "use strict";
 
-var eclipseServerApiAddr = "http://localhost:8080/bitcamp-web-18";
-var serverApiAddr = "http://localhost:8080/";
+var serverApiAddr = "http://localhost:8080";
 
 // DOM API가 완성 된 뒤에 실행 하라는 의미.
 $(() => {
-    $('footer').load(`${serverApiAddr}/html/footer.html`);
+    $('footer').load(`${serverApiAddr}/footer.html`);
 });
 
