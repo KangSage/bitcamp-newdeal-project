726f9f1ab2bdd0f31575dfe5762179a9b207aeb2
KangSage <ksage@knou.ac.kr>
2018-08-23 12:36:31 +0900

    2018.08.23_12:35 로그인 유효성 검사 완료.

--- src/main/webapp/html/signUp.js
+++ src/main/webapp/html/signUp.js
@@ -7,15 +7,58 @@ let authBtn = $('#auth-btn'),
     signUpPassword = $('#signUpPassword');
 
 
-authBtn.on('click', (e) => {
-    e.preventDefault();
+authBtn.on('click', () => {
     console.log("인증 버튼 눌림");
 });
 
 joinBtn.on('click', () => {
     console.log('회원가입 버튼 클릭');
-    $('.validate-form').trigger('validate-email');
+    let input = $('.validate-input .input100');
 
+    var check = false;
+
+    (function () {
+        check = true;
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) === false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+        return check;
+    })();
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+            hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') === 'email' || $(input).attr('name') === 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() === ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+        $(thisAlert).removeClass('alert-validate');
+    }
+
+if (check) {
     $.post(`${serverApiAddr}/json/member/signUp`, {
         'name': signUpName.val(),
         'email': signUpEmail.val(),
@@ -45,4 +88,5 @@ joinBtn.on('click', () => {
                 confirmButtonColor: "#e83e8c"
             });
         });
+    }
 });
